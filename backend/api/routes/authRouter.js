05fc4eed3e9cfc59d53cf9d1a0dd826f375975dd
Lavon Stalling <micah.shrak@gmail.com>
2019-06-07 12:34:30 -0700

    Upating migration file for users table

--- backend/api/routes/authRouter.js
+++ backend/api/routes/authRouter.js
@@ -2,4 +2,12 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const secrets = require('');
+const secrets = require('./secrets.js');
+
+const users = require('../database/dbModels/usersModel.js');
+
+// Registration route/endpoint
+router.post('/register', (req, res) => {
+	let user = re;
+	users.add(user);
+});
