24a8d8d81cd1654b5c490cf26758873dcb460858
Lavon Stalling <micah.shrak@gmail.com>
2019-06-07 12:54:15 -0700

    changes to migration file

--- backend/database/migrations/20190606121025_create_users_table.js
+++ backend/database/migrations/20190606121025_create_users_table.js
@@ -15,4 +15,6 @@ exports.up = function(knex, Promise) {
 	});
 };
 
-exports.down = function(knex, Promise) {};
+exports.down = function(knex, Promise) {
+	return knex.schema.dropIfTableExists('users');
+};
