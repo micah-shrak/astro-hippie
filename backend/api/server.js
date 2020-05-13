32d033e5ce579e8d08810a2b3218cc4daea28f5e
Lavon Stalling <micah.shrak@gmail.com>
2020-05-13 12:16:09 -0700

    adding middleware

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const server = express();
 
-server.use(express.json());
-server.use(helmet());
+server.use(express.json()); // used to parse JSON bodies
+server.use(helmet()); // helps to set HTTP headers for app protection
+server.use(morgan("combined")); // logger standard apache combines log output
 server.use(cors());
 
 // Check the server
-server.get('/', (req, res) => {
-	res.send('Coffee time...');
+server.get("/", (req, res) => {
+  res.send("Coffee time...");
 });
 
 // The rest of the routes/endpoints
-const { authRouter, usersRouter } = require('../api/index.js');
+const { authRouter, usersRouter } = require("../api/index.js");
 
 // Authorization/Authentication Route
-server.use('/auth', authRouter);
+server.use("/auth", authRouter);
 
 // Everthing referring to users this is the route
-server.use('/users', usersRouter);
+server.use("/users", usersRouter);
 
 module.exports = server;
